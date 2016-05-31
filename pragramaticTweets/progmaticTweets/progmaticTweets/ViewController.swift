dddb2448b9d562dd910c750f898cb08be1149868
leozhao0709 <zhao434@usc.edu>
2016-05-30 22:47:09 -0700

    chp09

--- pragramaticTweets/progmaticTweets/progmaticTweets/ViewController.swift
+++ pragramaticTweets/progmaticTweets/progmaticTweets/ViewController.swift
@@ -16,23 +16,7 @@ let defaultAvatarURL = NSURL(string: "https://abs.twimg.com/sticky/default_profi
 class ViewController: UITableViewController {
     
     
-    var parsedTweets: [ParsedTweet] = [
-        ParsedTweet(tweetText: "IOS 9 SDK Development now in print. Swift programming FTW!",
-            userName: "@pragprog",
-            createdAt: "2015-09-09 15:44:30 EDT",
-            userAvatarURL: defaultAvatarURL
-        ),
-        ParsedTweet(tweetText: "But was that really such a good idea?",
-            userName: "@redqueencoder",
-            createdAt: "2014-12-04 22:43:10 EDT",
-            userAvatarURL: defaultAvatarURL
-        ),
-        ParsedTweet(tweetText: "Struct all the things!",
-            userName: "@invalidname",
-            createdAt: "2015-07-31 12:32:29 EDT",
-            userAvatarURL: defaultAvatarURL
-        ),
-    ]
+    var parsedTweets: [ParsedTweet] = []
     
 
     override func viewDidLoad() {
@@ -95,7 +79,6 @@ class ViewController: UITableViewController {
     }
     
     @IBAction func handleRefresh(sender: AnyObject?) {
-        parsedTweets.append(ParsedTweet(tweetText: "New row", userName: "@refresh", createdAt: NSDate().description, userAvatarURL: defaultAvatarURL))
         self.reloadTweets()
         refreshControl?.endRefreshing()
     }
@@ -108,7 +91,24 @@ class ViewController: UITableViewController {
         NSLog("handleTwitterData(), \(data.length) bytes")
         do {
             let jsonObject = try NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions([]))
-            print("Json is \n \(jsonObject)")
+            guard let jsonArray = jsonObject as? [[String: AnyObject]] else {
+                NSLog("handleTwitterData() didn't get an array")
+                return
+            }
+            parsedTweets.removeAll()
+            for tweetDict in jsonArray {
+                var parsedTweet = ParsedTweet()
+                parsedTweet.tweetText = tweetDict["text"] as? String
+                parsedTweet.createdAt = tweetDict["created_at"] as? String
+                if let userDict = tweetDict["user"] as? [String: AnyObject] {
+                    parsedTweet.userName = userDict["name"] as? String
+                    if let avatarURLString = userDict["profile_image_url_https"] as? String {
+                        parsedTweet.userAvatarURL = NSURL(string: avatarURLString)
+                    }
+                }
+                parsedTweets.append(parsedTweet)
+            }
+            tableView.reloadData()
         } catch let error as NSError {
             NSLog("JSON error: \(error)")
         }
