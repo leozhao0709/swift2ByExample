0633cb582aaa29243aba6998ce4571f5c641885d
lzhao <lzhao@shutterfly.com>
2016-06-03 22:20:19 -0700

    continue

--- pragramaticTweets/progmaticTweets/progmaticTweets/TweetDetailViewController.swift
+++ pragramaticTweets/progmaticTweets/progmaticTweets/TweetDetailViewController.swift
@@ -34,6 +34,11 @@ class TweetDetailViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+//    override func viewWillAppear(animated: Bool) {
+//        super.viewWillAppear(animated)
+////        reloadTweetDetails()
+//    }
+    
 
     /*
     // MARK: - Navigation
@@ -46,7 +51,51 @@ class TweetDetailViewController: UIViewController {
     */
     
     func reloadTweetDetails() {
-        
+        guard let tweetIdString = tweetIdString else {
+            return
+        }
+        if let twitterAPIURL = NSURL(string: "https://api.twitter.com/1.1/statuses/show.json") {
+            let twitterParams = ["id": tweetIdString]
+            sendTwitterRequest(twitterAPIURL, params: twitterParams, completion: { (data, urlResponse, error) in
+                dispatch_async(dispatch_get_main_queue(), { 
+                    self.handleTwitterData(data, urlResponse: urlResponse, error: error)
+                })
+            })
+        }
+    }
+    
+    func handleTwitterData (data: NSData!, urlResponse: NSHTTPURLResponse!, error: NSError!) {
+        guard let data = data else{
+            NSLog("handleTwitterData() received no data")
+            return
+        }
+        NSLog("handleTwitterData(), \(data.length) bytes")
+        do {
+            let jsonObject = try NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions([]))
+            guard let tweetDict = jsonObject as? [String: AnyObject] else {
+                NSLog("handleTwitterData() didn't get a dictionary")
+                return
+            }
+//            print("tweetDict: \(tweetDict)")
+            self.tweetTextLabel.text = tweetDict["text"] as? String
+            if let userDict = tweetDict["user"] as? [String: AnyObject] {
+                self.userRealNameLabel.text = (userDict["name"] as! String)
+                self.userScreenNameLabel.text = (userDict["screen_name"] as! String)
+                self.userImageButton.setTitle(nil, forState: .Normal)
+                if let userImageURL = NSURL(string: userDict["profile_image_url_https"] as! String), userImageData = NSData(contentsOfURL: userImageURL) {
+                    self.userImageButton.setImage(UIImage(data: userImageData), forState: .Normal)
+                }
+                if let entities = tweetDict["entities"] as? [String: AnyObject],
+                media = entities["media"] as? [[String: AnyObject]],
+                mediaString = media[0]["media_url_https"] as? String,
+                mediaURL = NSURL(string: mediaString),
+                    mediaData = NSData(contentsOfURL: mediaURL) {
+                    tweetImageView.image = UIImage(data: mediaData)
+                }
+            }
+        } catch let error as NSError {
+            NSLog("JSON error: \(error)")
+        }
     }
 
 }
