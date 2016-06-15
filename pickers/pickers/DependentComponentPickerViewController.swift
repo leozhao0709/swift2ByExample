db8f6bcdb3d84432eaf49253c8190d2c2e05c9fb
leozhao0709 <zhao434@usc.edu>
2016-06-14 20:59:54 -0700

    finish pickers

--- pickers/pickers/DependentComponentPickerViewController.swift
+++ pickers/pickers/DependentComponentPickerViewController.swift
@@ -8,12 +8,28 @@
 
 import UIKit
 
-class DependentComponentPickerViewController: UIViewController {
+class DependentComponentPickerViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    private let stateComponent = 0
+    private let zipComponent = 1
+    private var stateZips: [String: [String]]!
+    private var states:[String]!
+    private var zips: [String]!
 
+    @IBOutlet weak var dependentPicker: UIPickerView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        let bundle = NSBundle.mainBundle()
+        let plistURL = bundle.URLForResource("statedictionary", withExtension: "plist")
+        stateZips = NSDictionary(contentsOfURL: plistURL!) as! [String: [String]]
+        let allStates = stateZips.keys
+        states = allStates.sort()
+        
+        let selectedState = states[0]
+        zips = stateZips[selectedState]!
     }
 
     override func didReceiveMemoryWarning() {
@@ -31,5 +47,62 @@ class DependentComponentPickerViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    @IBAction func onButtonPressed(sender: UIButton) {
+        let stateRow = dependentPicker.selectedRowInComponent(stateComponent)
+        let zipRow = dependentPicker.selectedRowInComponent(zipComponent)
+        
+        let state = states[stateRow]
+        let zip = zips[zipRow]
+        
+        let title = "You selected zip code \(zip)"
+        let message = "\(zip) is in \(state)"
+        
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .Alert)
+        let action = UIAlertAction(title: "OK", style: .Default, handler: nil)
+        alert.addAction(action)
+        presentViewController(alert, animated: true, completion: nil)
+    }
+    
+    //MARK: - Picker Delegate Methods
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        if component == stateComponent {
+            return states[row]
+        } else {
+            return zips[row]
+        }
+    }
+    
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        if component == stateComponent {
+            let selectedState = states[row]
+            zips = stateZips[selectedState]!
+            dependentPicker.reloadComponent(zipComponent)
+            dependentPicker.selectRow(0, inComponent: zipComponent, animated: true)
+        }
+    }
+    
+    func pickerView(pickerView: UIPickerView, widthForComponent component: Int) -> CGFloat {
+        let pickerWidth = pickerView.bounds.size.width
+        if component == zipComponent {
+            return pickerWidth/3
+        } else {
+            return 2 * pickerWidth/3
+        }
+    }
+    
+    //MARK: - Picker Data Source Methods
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 2
+    }
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if component == stateComponent {
+            return states.count
+        } else {
+            return zips.count
+        }
+    }
+    
 
 }
