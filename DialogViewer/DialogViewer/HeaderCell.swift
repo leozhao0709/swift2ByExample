186403e5c34bad26d9ccd16183fdd1678a232502
lzhao <lzhao@shutterfly.com>
2016-06-27 18:40:11 -0700

    collection view

---
+++ DialogViewer/DialogViewer/HeaderCell.swift
@@ -0,0 +1,26 @@
+//
+//  HeaderCell.swift
+//  DialogViewer
+//
+//  Created by Lei Zhao on 6/27/16.
+//  Copyright © 2016 Lei Zhao. All rights reserved.
+//
+
+import UIKit
+
+class HeaderCell: ContentCell {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        label.backgroundColor = UIColor(red: 0.9, green: 0.9, blue: 0.8, alpha: 1.0)
+        
+        label.textColor = UIColor.blackColor()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override class func defaultFont() -> UIFont {
+        return UIFont.preferredFontForTextStyle(UIFontTextStyleHeadline)
+    }
+}
--- DialogViewer/DialogViewer/ViewController.swift
+++ DialogViewer/DialogViewer/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UICollectionViewController {
+class ViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
 
     private var sections = [
         ["header": "First Witch", "content": "Hey, when will the three of us meet up later?"],
@@ -25,7 +25,56 @@ class ViewController: UICollectionViewController {
         var contentInset = collectionView?.contentInset
         contentInset?.top = 20
         collectionView?.contentInset = contentInset!
+        
+        let layout = collectionView!.collectionViewLayout
+        let flow = layout as! UICollectionViewFlowLayout
+        flow.sectionInset = UIEdgeInsetsMake(10, 20, 30, 20)
+        
+        flow.headerReferenceSize = CGSizeMake(100, 25)
+        
+        collectionView?.registerClass(HeaderCell.self, forSupplementaryViewOfKind: UICollectionElementKindSectionHeader, withReuseIdentifier: "HEADER")
     }
-
+    
+    override func collectionView(collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, atIndexPath indexPath: NSIndexPath) -> UICollectionReusableView {
+        if(kind == UICollectionElementKindSectionHeader) {
+            let cell = collectionView.dequeueReusableSupplementaryViewOfKind(kind, withReuseIdentifier: "HEADER", forIndexPath: indexPath) as! HeaderCell
+            cell.maxWidth = collectionView.bounds.size.width
+            cell.text = sections[indexPath.section]["header"]
+            return cell
+        }
+        abort()
+    }
+    
+    func wordsInSection(section: Int)->[String] {
+        let content = sections[section]["content"]
+        let spaces = NSCharacterSet.whitespaceAndNewlineCharacterSet()
+        let words = content?.componentsSeparatedByCharactersInSet(spaces)
+        return words!
+    }
+    
+    override func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return sections.count
+    }
+    
+    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        let words = wordsInSection(section)
+        return words.count
+    }
+    
+    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let words = wordsInSection(indexPath.section)
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("CONTENT", forIndexPath: indexPath) as! ContentCell
+        cell.maxWidth = collectionView.bounds.size.width
+        cell.text = words[indexPath.row]
+        return cell
+    }
+    
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+        let words = wordsInSection(indexPath.section)
+        let size = ContentCell.sizeForContentString(words[indexPath.row], forMaxWidth: collectionView.bounds.size.width)
+        return size
+        
+        
+    }
 }
 
