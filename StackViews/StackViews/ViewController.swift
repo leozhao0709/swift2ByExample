c5d2d037c38e763020fd79632aee55b3b1947ea8
leozhao0709 <zhao434@usc.edu>
2016-07-01 00:23:08 -0700

    stack view initial

---
+++ StackViews/StackViews/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StackViews
+//
+//  Created by Lei Zhao on 6/30/16.
+//  Copyright © 2016 Lei Zhao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
